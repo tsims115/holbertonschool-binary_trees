6b95b7837b16f4755647d3f7e2f4d01e7421f9d7
Trenton Sims <tsims115@yahoo.com>
2022-08-01 16:55:28 -0500

    Adding task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,15 +2,18 @@
 /**
  * binary_tree_node - creates binary tree node
  * 
- * @parent: 
- * @value: 
+ * @parent: parent of node to be created
+ * @value: value to insert into the node
  * Return: Returns pointer to the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
+	
 	new_node->n = value;
 	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	return new_node;
 }
