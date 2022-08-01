dc657e2917e96226dde6dfa7ad8fe202cce411d5
Trenton Sims <tsims115@yahoo.com>
2022-08-01 18:37:49 -0500

    Adding task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts node to left of parent node
+ *
+ * @parent: parent node
+ * @value: value to insert
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node || parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
