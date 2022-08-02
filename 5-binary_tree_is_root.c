54b06a8812c889fa6fc6fd076f5f434a1ef429fb
Trenton Sims <tsims115@yahoo.com>
2022-08-01 19:24:00 -0500

    Adding task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_root - check if node is root
- * 
- * @node: node to check 
+ *
+ * @node: node to check
  * Return: Return 1 if root and 0 if not
  */
 int binary_tree_is_root(const binary_tree_t *node)
