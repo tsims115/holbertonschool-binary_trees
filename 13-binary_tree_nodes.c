6bab816868fc3d9b45a6c95f458c2609ffac9754
Trenton Sims <tsims115@yahoo.com>
2022-08-01 22:07:53 -0500

    Adding task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,10 +14,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		lsize = binary_tree_nodes(tree->left);
+		lsize += binary_tree_nodes(tree->left);
 	if (tree->right)
-		rsize = binary_tree_nodes(tree->right);
-	if (!tree->right || !tree->left)
+		rsize += binary_tree_nodes(tree->right);
+	if (tree->right || tree->left)
 		return (lsize + rsize + 1);
 	return (lsize + rsize);
 }
