812ba8f8b4175490eb63bc86583080ed5fb77c83
Trenton Sims <tsims115@yahoo.com>
2022-08-01 22:22:45 -0500

    Adding task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - check if binary tree is full
+ * 
+ * @tree: tree to use 
+ * Return: return 1 if full and 0 if not or null 
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int full;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		full = binary_tree_is_full(tree->left);
+	if (tree->right)
+		full = binary_tree_is_full(tree->right);
+	if (full == 0)
+		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
+	if (!tree->right || !tree->left)
+		return (0);
+	return (1);
+}
