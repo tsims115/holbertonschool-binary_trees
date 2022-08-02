4b136e6eee7c7d2b9a00cff6593a8ffca3ae087d
Trenton Sims <tsims115@yahoo.com>
2022-08-01 20:36:45 -0500

    ADding task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - traverse useing preorder
+ * 
+ * @tree: tree to use
+ * @func: function to use
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+}
