ed2f80181f7faf8ebddd6170597c780e3bcfa620
Trenton Sims <tsims115@yahoo.com>
2022-08-01 21:07:09 -0500

    Adding task 8

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures height of binary tree
+ * 
+ * @tree: tree to use
+ * Return: size of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lsize = 0;
+	size_t rsize = 0;
+	size_t maxsize;
+
+	if (!tree)
+		return (-1);
+	lsize = binary_tree_height(tree->left) + 1;
+	rsize = binary_tree_height(tree->right) + 1;
+	if (lsize > rsize)
+		maxsize = lsize;
+	else
+		maxsize = rsize;
+	return (maxsize);
+}
