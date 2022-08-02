e9267f58e972b652e8f3dbe6b664655a89df1b3d
Trenton Sims <tsims115@yahoo.com>
2022-08-02 01:36:08 -0500

    Adding task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - check for sibling if one
+ *
+ * @node: node to check for sibling
+ * Return: pointer to the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *cur_node;
+	if (!node || !node->parent)
+		return (NULL);
+	cur_node = node->parent;
+	if (cur_node->left == node)
+		return cur_node->right;
+	return cur_node->left;
+}
