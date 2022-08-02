14d80be4394a7eef98b617e10eafbf5c175ef145
Trenton Sims <tsims115@yahoo.com>
2022-08-01 21:15:50 -0500

    Adding task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,13 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t maxsize;
 
 	if (!tree)
-	{
-		if(!tree && !tree->parent)
-			return (0);
-		return (-1);
-	}
-	lsize = binary_tree_height(tree->left) + 1;
-	rsize = binary_tree_height(tree->right) + 1;
+		return (0);
+	if (tree->left)
+		lsize = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		rsize = binary_tree_height(tree->right) + 1;
 	if (lsize > rsize)
 		maxsize = lsize;
 	else
