f71bfb3725d4cf4b6625a40949052255cd5dc94d
Trenton Sims <tsims115@yahoo.com>
2022-08-02 01:37:05 -0500

    Adding task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,11 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *cur_node;
+
 	if (!node || !node->parent)
 		return (NULL);
 	cur_node = node->parent;
 	if (cur_node->left == node)
-		return cur_node->right;
-	return cur_node->left;
+		return (cur_node->right);
+	return (cur_node->left);
 }
