2936816e71f83783dafd6dea21ed62c7dc04a246
Trenton Sims <tsims115@yahoo.com>
2022-08-01 22:40:27 -0500

    Adding task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_height - measures height of binary tree
+ *
+ * @tree: tree to use
+ * Return: size of tree
+ */
+int _binary_tree_height(const binary_tree_t *tree)
+{
+	int lsize = 0;
+	int rsize = 0;
+	int maxsize;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		lsize += _binary_tree_height(tree->left);
+	if (tree->right)
+		rsize += _binary_tree_height(tree->right);
+	if (lsize > rsize)
+		maxsize = lsize;
+	else
+		maxsize = rsize;
+	return (maxsize + 1);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of tree
  *
  * @tree: tree to use
@@ -8,14 +33,8 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int lsize = 0;
-	int rsize = 0;
-
 	if (!tree)
 		return (0);
-	if (tree->left)
-		lsize = binary_tree_balance(tree->left) + 1;
-	if (tree->right)
-		rsize = binary_tree_balance(tree->right) + 1;
-	return (lsize - rsize);
+	
+	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
