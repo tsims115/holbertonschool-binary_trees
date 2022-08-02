b19b725f9c75c39660eb0a304e12c40cd518f958
Trenton Sims <tsims115@yahoo.com>
2022-08-01 21:25:34 -0500

    Adding task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measure depth of a node in a binary tree
+ *
+ * @tree: tree to use
+ * Return: depth of node in tree 
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t size;
+	const binary_tree_t *cur_node = tree;
+	if (!tree || !tree->parent)
+		return (0);
+	size = binary_tree_depth(tree->parent);
+	return (size + 1);
+}
