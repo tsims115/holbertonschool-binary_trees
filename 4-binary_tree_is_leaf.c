455f35ff259451dd5ff2b4ef3311ba9860b5e039
Trenton Sims <tsims115@yahoo.com>
2022-08-01 19:11:10 -0500

    ADding task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,13 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf
- * 
- * @node: node to check if leaf 
- * Return: 1 if leaf and 0 if not 
+ * binary_tree_is_leaf - checks if node is a leaf
+ *
+ * @node: node to check if leaf
+ * Return: 1 if leaf and 0 if not
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (!node)
+		return (0);
 	if (!node->left && !node->right)
 		return (1);
 	return (0);
