c083df4f5f71f90852e80f327b06e82cdf705319
Trenton Sims <tsims115@yahoo.com>
2022-08-01 21:58:51 -0500

    Adding task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with atleast 1 child node
+ * 
+ * @tree: tree to use 
+ * Return: number of nodes with atleast one child 
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t lsize = 0;
+	size_t rsize = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		lsize = binary_tree_leaves(tree->left);
+	if (tree->right)
+		rsize = binary_tree_leaves(tree->right);
+	if (!tree->right || !tree->left)
+		return (lsize + rsize + 1);
+	return (lsize + rsize);
+}
