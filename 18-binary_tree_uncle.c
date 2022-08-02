7f30f4282a8b6481ed1582324bc9fc3f69f72f06
Trenton Sims <tsims115@yahoo.com>
2022-08-02 01:44:23 -0500

    Adding task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - finds uncle
+ *
+ * @node: node to use
+ * Return: poitner
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *cur_node;
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	cur_node = node->parent->parent;
+	if (cur_node->left == node->parent)
+		return (cur_node->right);
+	return (cur_node->left);
+}
