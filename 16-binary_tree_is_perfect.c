842effe8a48961824b92cbb8c873f6953e6fb3f2
Trenton Sims <tsims115@yahoo.com>
2022-08-02 01:25:09 -0500

    Adding task 15

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures size of binary tree
+ *
+ * @tree: tree to use
+ * Return: size of tree
+ */
+int _binary_tree_size(const binary_tree_t *tree)
+{
+	int lsize = 0;
+	int rsize = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		lsize = _binary_tree_size(tree->left);
+	if (tree->right)
+		rsize = _binary_tree_size(tree->right);
+	return (lsize + rsize + 1);
+}
+
+
+/**
+ * binary_tree_is_perfect
+ *
+ * @tree: tree to use
+ * Return: int if perfect
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int lsize, rsize;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		lsize = _binary_tree_size(tree->left);
+	if (tree->right)
+		rsize = _binary_tree_size(tree->right);
+	if (lsize == rsize)
+		return (1);
+	return (0);
+}
