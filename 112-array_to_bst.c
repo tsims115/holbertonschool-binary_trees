83aa4d5c3eb587a2d16dad08fb143c74bd4f0119
Trenton Sims <tsims115@yahoo.com>
2022-08-10 00:07:19 -0500

    Adding task 2

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,23 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * value_check - checks if value is in array
- *
- * @a: array to check
- * @value: value to chekc for
- * Return: 1 if it is and 0 if not
- */
-int value_check(int *a, int value, size_t size)
-{
-	size_t i = 0;
-
-	for (i = 0; i < size; i++)
-		if (a[i] == value)
-			return (1);
-	return (0);
-}
-
-/**
  * array_to_bst - array to a binary search tree
  *
  * @array: array to use
@@ -26,18 +9,14 @@ int value_check(int *a, int value, size_t size)
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-	int *a_tmp = malloc(sizeof(int) * size);
 	int i;
 	bst_t *root = NULL;
 
+	if (!array)
+		return NULL;
 	for (i = 0; i < (int)size; i++)
 	{
-		if (value_check(a_tmp, array[i], size) == 0)
-		{
-			if (!bst_insert(&root, array[i]))
-				return(NULL);
-		}
+		bst_insert(&root, array[i]);
 	}
-	free(a_tmp);
 	return (root);
 }
